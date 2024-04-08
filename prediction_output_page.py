be1f351ffe56d15ab1cfb46eec9f294145656556
SumantKK <166042322+SumantKK@users.noreply.github.com>
2024-04-08 16:27:39 +0530

    Delete prediction_output_page.py

--- prediction_output_page.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def show_prediction_output(prediction):
-    st.title('Prediction Output')
-    if prediction is not None:
-        Value_Estimate = int(round(prediction[0]))
-        st.write('Total Quantity (30 Kg Multiples or Bags) :', Value_Estimate)
-        st.write('Total Weight :', 30 * Value_Estimate)
-        st.write('Estimated Area of Sq Meters it will cover:', int(round(30 * Value_Estimate / 7.5)))
